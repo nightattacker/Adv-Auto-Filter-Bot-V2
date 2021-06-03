80b78794781261d9c7393c0023e32aff2598fea9
nightattacker <84122155+nightattacker@users.noreply.github.com>
2021-06-03 12:35:32 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1639,10 +1639,10 @@ async def callback_data(bot, update: CallbackQuery):
 
     if query_data == "start":
         buttons = [[
-            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/AlbertEinstein_TG'),
-            InlineKeyboardButton('Source Code 🧾', url ='https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot')
+            InlineKeyboardButton('My Dev 👨‍🔬', url='https://t.me/nightattacker111'),
+            InlineKeyboardButton('Source Code 🧾', url ='https://github.com/nightattacker/Adv-Auto-Filter-Bot')
         ],[
-            InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+            InlineKeyboardButton('Join Channel', url='https://t.me/joinchat/WaAK-tb5GIw5MGE1')
         ],[
             InlineKeyboardButton('Help ⚙', callback_data="help")
         ]]
